ec1415b5e3783f38af8fbde567d5a6dcd0f54e91
Ritesh Roshan Sahoo <riteshroshansahoo01@gmail.com>
2026-05-05 07:38:28 +0530

    chore: deep clean old v1 files to fix vercel 403

---
+++ v2/.next/dev/server/pages/_document.js
@@ -0,0 +1,4 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
+R.c("server/chunks/ssr/0cm__next_document_0zbbngi.js")
+R.m("[project]/v2/node_modules/next/document.js [ssr] (ecmascript)")
+module.exports=R.m("[project]/v2/node_modules/next/document.js [ssr] (ecmascript)").exports
---
+++ v2/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_0~pg0mt._.js",
+  "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
+  "static/chunks/node_modules_next_dist_0k8zr~w._.js",
+  "static/chunks/0cm__next_dist_pages__error_0zz.jhx.js",
+  "static/chunks/[next]_entry_page-loader_ts_0qtlpij._.js",
+  "static/chunks/node_modules_react-dom_0bruynb._.js",
+  "static/chunks/node_modules_0lx093h._.js",
+  "static/chunks/[root-of-the-server]__12a_nk0._.js",
+  "static/chunks/v2_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-v2_pages__error_0~9qk6r._.js"
+])
